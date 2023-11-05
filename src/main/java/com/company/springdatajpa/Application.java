c0f689db6e061b84e9f61a73647f547a4ef7919c
Asus <bpardaboyev2000@gmail.com>
2023-11-05 10:45:29 +0500

    hateos learned

--- src/main/java/com/company/springdatajpa/Application.java
+++ src/main/java/com/company/springdatajpa/Application.java
@@ -1,13 +1,37 @@
 package com.company.springdatajpa;
 
+import com.company.springdatajpa.post.Post;
+import com.company.springdatajpa.post.PostRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.HateoasPageableHandlerMethodArgumentResolver;
+import org.springframework.data.web.PagedResourcesAssembler;
+
+import java.net.URL;
+import java.util.List;
 
 @SpringBootApplication
 
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean(name = "postPagedResourcesAssembler")
+    public PagedResourcesAssembler<Post> postPagedResourcesAssembler() {
+        return new PagedResourcesAssembler<>(new HateoasPageableHandlerMethodArgumentResolver(), null);
+    }
+    @Bean
+    CommandLineRunner runner(ObjectMapper objectMapper, PostRepository postRepository) {
+        return (args -> {
+            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+            List<Post> posts = objectMapper.readValue(url, new TypeReference<List<Post>>() {
+            });
+            postRepository.saveAll(posts);
+        });
     }
 
 }
