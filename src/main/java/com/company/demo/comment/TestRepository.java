7e4c9dba67b30dd3ecfab9f7a5040635dfb3f9a5
Asus <bpardaboyev2000@gmail.com>
2023-11-05 16:42:17 +0500

    spring doc

---
+++ src/main/java/com/company/demo/comment/TestRepository.java
@@ -0,0 +1,8 @@
+package com.company.demo.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, Integer> {
+}
