bcce5d0074c22bea60c78ac857b8df8814e5b44c
Asus <bpardaboyev2000@gmail.com>
2023-11-05 12:06:25 +0500

    data rest

--- src/main/java/com/company/springdatajpa/PostRepository.java
+++ src/main/java/com/company/springdatajpa/PostRepository.java
@@ -1,6 +1,11 @@
 package com.company.springdatajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
+@RepositoryRestResource(path = "ketmonlar",collectionResourceRel = "bolta")
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    Optional<Post> findByTitleIgnoreCase(String title);
 }
